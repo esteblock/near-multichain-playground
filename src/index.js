b0b626c4ffa4fe9c7e7c33a25296f8c322f884d0
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-03-22 14:31:05 -0300

    set wallet

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("🚀 ~ Hello World")
+import nearAPI from 'near-api-js';
+
+export async function init() {
+    // creates keyStore from a private key string
+    // you can define your key here or use an environment variable
+    
+    const { keyStores, KeyPair } = nearAPI;
+    const myKeyStore = new keyStores.InMemoryKeyStore();
+    const PRIVATE_KEY =
+      "by8kdJoJHu7uUkKfoaLd2J2Dp1q1TigeWMG123pHdu9UREqPcshCM223kWadm";
+    // creates a public / private key pair using the provided private key
+    const keyPair = KeyPair.fromString(PRIVATE_KEY);
+    // adds the keyPair you created to keyStore
+    await myKeyStore.setKey("testnet", "example-account.testnet", keyPair);
+    
+    console.log("🚀 ~ Hello World")
+
+}
+await init();
+
