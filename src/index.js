e42c40c63455edbcb0612f93eb96468ecb78f6cf
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-03-22 15:17:18 -0300

    define wallet

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ export async function init() {
     // creates keyStore from a private key string
     // you can define your key here or use an environment variable
     
-    const { keyStores, KeyPair } = nearAPI;
+    const { keyStores, KeyPair, connect, WalletConnection } = nearAPI;
     const myKeyStore = new keyStores.InMemoryKeyStore();
     const SEED_PHRASE = process.env.SEED_PHRASE;
     const ACCOUNT_NAME = process.env.ACCOUNT_NAME;
@@ -15,6 +15,33 @@ export async function init() {
     const keyPair = KeyPair.fromString(secretKey);
     // adds the keyPair you created to keyStore
     await myKeyStore.setKey("testnet", "example-account.testnet", keyPair);
+
+    const receiver = "0xe0f3B7e68151E9306727104973752A415c2bcbEb";
+    const amount = 0.01;
+
+    
+    const connectionConfig = {
+        networkId: "testnet",
+        keyStore: myKeyStore, // first create a key store
+        nodeUrl: "https://rpc.testnet.near.org",
+        walletUrl: "https://testnet.mynearwallet.com/",
+        helperUrl: "https://helper.testnet.near.org",
+        explorerUrl: "https://testnet.nearblocks.io",
+    };
+    const nearConnection = await connect(connectionConfig);
+     
+    // wallet.value = new WalletConnection(
+    //     near.value,
+    //     `${appKeyPrefix.value}-${networkId}`
+    //   );
+
+    const appKeyPrefix = "";
+    const networkId= "testnet"
+
+    //   // create wallet connection
+      const walletConnection = new WalletConnection(
+        nearConnection,
+        `${appKeyPrefix.value}-${networkId}`);
 
 }
 await init();
